3376fd717ee874703c1cc1116274cbf00da94949
Amalia Andreea Gurau <amalia.gurau@emmanuel.ro>
2022-01-13 16:22:25 +0200

    recursiv3

---
+++ Recursivitate3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recursivitate3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Recursivitate.Fibonacci(0, 1, 1, 5));
+        }
+    }
+}
---
+++ Recursivitate3/Recursivitate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate3
+{
+    class Recursivitate
+    {
+        public static int Fibonacci(int a, int b, int n, int p)
+        {
+            if (n == p)
+                return a;
+            else
+                return Fibonacci(b, a + b, n + 1, p);
+
+
+        }
+    }
+}
